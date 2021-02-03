6fd41743b32b6248f6c7aee7b8efb647f5e9a148
emmafewer <emmafewer20@gmail.com>
2021-02-02 17:09:14 -0800

    done

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -11,10 +11,7 @@ class DelayedButton extends Component {
     
     render () {
         return (
-            <button
-                onClick={this.handleClick}
-            >
-            </button>
+            <button onClick={this.handleClick}></button>
         )
     }
 }
